c59e0b4453b7b8d6f46b380b84e72082e2e90700
Neal Venditto <Neal.Venditto@unnpp.gov>
2020-12-10 14:01:27 -0500

    Updated for 'basic' package v1.0.1

--- cmake/projects/basic/hunter.cmake
+++ cmake/projects/basic/hunter.cmake
@@ -12,11 +12,11 @@ hunter_add_version(
     PACKAGE_NAME
     basic
     VERSION
-    1.0.0
+    1.0.1
     URL
-    "https://github.com/nvenditto/basic/archive/v1.0.0.tar.gz"
+    "https://github.com/nvenditto/basic/archive/v1.0.1.tar.gz"
     SHA1
-    8EB0D232DB1369328B8FF0CA518C7B32F5412171
+    36E16804A6115732042130812286EADB70EB0D32
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
